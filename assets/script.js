06baf8ebb9968a8b162f06e34d9a51b756ea8526
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-01-05 16:46:35 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,25 @@ const navBtn = document.getElementById('user-menu-button');
 function toggleText() {
   if (displayNav.style.display === "block") {
     displayNav.style.display = "none";
-  
-  } else {
-    displayNav.style.display = "block";
 
+  } else {
+   
+ displayNav.style.display = "block";
+  }
+}
+
+const displayP = document.querySelector('#btnText')
+let p = document.getElementById('clickText');
+
+
+function toggleP() {
+  if (p.style.display === "block") {
+    p.style.display = "none";
+    displayP.textContent = "Click for details";
+  } else {
+    p.style.display = "block";
+
+    displayP.textContent = "Click to minimize";
   }
 }
 
