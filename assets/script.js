bc0335653af89982f67b45cc137348833a899344
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-01-27 12:22:54 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
-console.log("How are u")
-
-const displayNav = document.querySelector('#DropDown')
-const navBtn = document.getElementById('user-menu-button');
-
 document.querySelector('.drop').style.display = 'none';
+
+const displayNav = document.querySelector('#DropDown');
+const navBtn = document.getElementById('user-menu-button');
+const dropBtn = document.getElementById('btn-drop') ;
+const dropMenu = document.querySelector('first');
+const toggleDark = document.querySelector('colors')
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
 function toggleText() {
   if (displayNav.style.display === "block") {
@@ -30,3 +32,19 @@ function toggleP() {
   }
 }
 
+if (viewportWidth > 650) {
+	console.log('Wide viewport');
+
+} else {
+	console.log('Small viewport');
+  dropSvg.addEventListener('keydown', append())
+}
+
+
+function append(){
+  dropMenu.append(displayNav);
+}
+
+toggleDark.addEventListener('click', function(){ 
+    document.body.classList.toggle('dark_mode');
+});
