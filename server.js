758ded868f71d9df6c418db80c669d3e9c4ebe63
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-03-24 09:39:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // res.sendFile(path.join(__dirname, '../public', 'index.html'));
 app.get('/', function(req, res) {
