18f38b68f2ef95274679f08e1820f3cd3c3caeba
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-03-24 09:16:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static("public/index.html"));
 
-res.sendFile(path.join(__dirname, '../public', 'index.html'));
+// res.sendFile(path.join(__dirname, '../public', 'index.html'));
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
